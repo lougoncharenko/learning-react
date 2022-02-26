b0967c62801ce673a918005a1c2b66bf72164c2f
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-25 20:33:10 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -8,20 +8,29 @@ function App(){
     {id: 2, text: 'Comment Two'},
     {id: 3, text: 'Comment Three'}
   ]
+
+  const loading= false
+  const showComments= true
+  const commentBlock=   <div className="comments">
+  <h3>Comments ({comments.length})</h3>
+  <ul>
+    {comments.map((comment, index) => (
+      <li key={index}>{comment.text}</li>
+
+    ))}
+  </ul> 
+  </div>
+
+  if(loading) return <h1>Loading...</h1>
   return  (
   <div className='container'> 
     <h1>{title}</h1>
     <p>{body}</p>
-    
-   <div className="comments">
-     <h3>Comments ({comments.length})</h3>
-     <ul>
-       {comments.map((comment, index) => (
-         <li>{comment.text}</li>
 
-       ))}
-     </ul>
-   </div>
+    {showComments && commentBlock}
+    
+ 
+   
 
 
 
