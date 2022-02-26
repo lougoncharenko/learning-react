7d31d801fa375af866c1f1e2fe495044f516d0c6
lougoncharenko <93098869+lougoncharenko@users.noreply.github.com>
2022-02-25 20:21:22 -0500

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,32 @@
+
+
 function App(){
-  return  <h1>Hello from the App component</h1>
+  const title='Blog post'
+  const body='This is my blog post'
+  const comments=[
+    {id: 1, text: 'Comment One'},
+    {id: 2, text: 'Comment Two'},
+    {id: 3, text: 'Comment Three'}
+  ]
+  return  (
+  <div className='container'> 
+    <h1>{title}</h1>
+    <p>{body}</p>
+    
+   <div className="comments">
+     <h3>Comments ({comments.length})</h3>
+     <ul>
+       {comments.map((comment, index) => (
+         <li>{comment.text}</li>
+
+       ))}
+     </ul>
+   </div>
+
+
+
+    </div>
+  )
 }
 
 export default App
